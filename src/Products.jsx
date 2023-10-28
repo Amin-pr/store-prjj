1bb997ab9f79455c77dd081332543c2cba92b839
Amin-pr <127015189+Amin-pr@users.noreply.github.com>
2023-10-28 10:10:12 +0330

    6

--- src/Products.jsx
+++ src/Products.jsx
@@ -140,14 +140,14 @@ function Products({
               <div class="container card border-1 m-2 p-2 d-flex ">
                 <img
                   src={Data.image}
-                  class="banner-image card-image-top border-0"
+                  class="banner-image card-image-top border-0 h-50"
                   alt="product"
                 />
-                <div class="wrapper ">
-                  <p> {Data.title}</p>
-                  <p className="text-start h5">{Data.price}$</p>
+                <div class="wrapper d-flex flex-wrap align-bot">
+                  <p className="my-3 mx-1 "  > {Data.title}</p>
+                  <p className="text-start h5 my-3 mx-1">{Data.price}$</p>
                 </div>
-                <div class="button-wrapper align-self-end">
+                <div class="button-wrapper align-self-end ">
                   <button
                     class="btn outline"
                     onClick={() => productHandler(Data)}
@@ -156,7 +156,7 @@ function Products({
                     DETAILS
                   </button>
                   <button
-                    class="btn fill"
+                    class="btn "
                     onMouseEnter={() => setProductInfo(Data)}
                     onClick={() => addHandler(Data)}
                   >
